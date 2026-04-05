b9e797cf1d4b5ab12b1470b8a48c3d79f17148d2
Kosuke Muraki <kosuke.murakifs@gmail.com>
2026-04-05 15:23:13 +0900

    feat: ソリューション構成とDomain層の作成

---
+++ NewsHub.Domain/Entities/NewsArticle.cs
@@ -0,0 +1,24 @@
+﻿namespace NewsHub.Domain.Entities
+{
+    public class NewsArticle
+    {
+        /// <summary>タイトル</summary>
+        public string? Title { get; set; }
+
+        /// <summary>説明文</summary>
+        public string? ExplanatoryText { get; set; }
+
+        /// <summary>記事のURL</summary>
+        public string? ArticleUrl { get; set; }
+
+        /// <summary>画像のURL</summary>
+        public string? ImageUrl{ get; set; }
+
+        /// <summary>ニュースソース名</summary>
+        public string? SourceName { get; set; }
+
+        /// <summary>公開日時</summary>
+        public DateTime PublishedDate { get; set; }
+
+    }
+}
---
+++ NewsHub.Domain/Interfaces/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using NewsHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsHub.Domain.Interfaces
+{
+    internal interface INewsService
+    {
+        /// <summary>
+        /// NewsAPIより記事を取得する
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        Task<List<NewsArticle>> SearchAsync(string keyword);
+    }
+}
