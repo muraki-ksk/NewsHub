6dbc188ec61c82e8e74bf69bbeb083ccf7d152f2
Kosuke Muraki <kosuke.murakifs@gmail.com>
2026-04-07 15:52:30 +0900

    アプリケーション層の作成完了

---
+++ NewsHub.Application/UseCases/SearchNewsUseCase.cs
@@ -0,0 +1,26 @@
+﻿using NewsHub.Domain.Entities;
+using NewsHub.Domain.Interfaces;
+
+namespace NewsHub.Application.UseCases
+{
+    public class SearchNewsUseCase
+    {
+        private readonly INewsService _newsService;
+
+        public SearchNewsUseCase(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        public async Task<List<NewsArticle>> ExecuteAsync(string keyword)
+        {
+            // キーワードが空なら空リストを返す
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                return new List<NewsArticle>();
+            }
+
+            return await _newsService.SearchAsync(keyword);
+        }
+    }
+}
